2bab9629b93d6e4da39311d06fa7f3ec912b4f50
Antonio Pedro Paulo <antoniopaulokassomah@gmail.com>
2023-10-09 08:04:06 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+var ul =document.querySelector('nav ul')
+function nome(){
+  if(ul.classList.contains('open')){
+      ul.classList.remove('open')
+     }else{
+      ul.classList.add('open')
+     }
+}
